4b943aef5336aef92d2ce879c24bb2a2620fae3b
Marceloalvarez12 <71199245+Marceloalvarez12@users.noreply.github.com>
2023-03-08 14:54:08 -0300

    Inspeccionar con los botones para elegir defectos y par de primera para aumenta y disminuir

---
+++ QCS/Controllers/InspeccionController.cs
@@ -0,0 +1,45 @@
+﻿using Datos;
+using Microsoft.Ajax.Utilities;
+using Negocio.Interfaces;
+using Negocio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace QCS.Controllers
+{
+    public class InspeccionController : Controller
+    {
+
+        private IRepoDefecto repoDefecto;
+        public InspeccionController()
+        {
+            if (repoDefecto == null)
+            {
+                repoDefecto = new RepoDefecto();
+            }
+
+        }
+        // GET: Inspeccion
+        public ActionResult Index()
+        {
+            var defectos = repoDefecto.GetDefectos();
+            ViewData.Model = defectos;
+            return View();
+        }
+
+        public ActionResult MyView(int currentValue)
+        {
+           
+            ViewBag.CurrentValue = currentValue;
+            return View();
+        }
+
+    }
+}
+
